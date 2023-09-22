47c36a22b397d5a98128773d1a70347c2742eab2
Brösztl Máté 826 <bromat826@hengersor.hu>
2023-09-22 12:59:06 +0200

    made some change

--- webs/app.js
+++ webs/app.js
@@ -1,12 +1,17 @@
 document.getElementById("myButton").onclick = function(){
-    var Knev = document.getElementById("fname").value
-    let Vnev = document.getElementById("lname").value
-    let email = document.getElementById("email").value
-    let message = document.getElementById("message")
-
-
-    let lenght = Knev.lenght;
-    if (lenght < 1) {
-        alert("Write your name!!")
+    if (String(document.getElementById("name").value).length < 1) {
+        alert("Please write down your name.")
     }
+    else if(String(document.getElementById("email").value).length < 1){
+        alert("Please write down your email.")
+    }
+    else if(String(document.getElementById("phone").value).length < 1){
+        alert("Please write down your phone number.")
+    }
+    else if(String(document.getElementById("message").value).length < 1){
+        alert("Please write down how can we help you.")
+    }
+    else{
+        alert("The message is send. We will contact you as soon as we know the answer your problem.")
+    } 
 }
